18465e9dcf44cc02d381b93ea2e2320af0a9ad37
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-10-01 20:52:32 +0530

    made more changes

--- dataLoader.py
+++ dataLoader.py
@@ -10,26 +10,32 @@ def load_from_folder(
     images = []
     masks = []
     for i in range(1, quantity):
+        print(i)
         folder = f"{i:03d}"
         folder = os.path.join(imgFolder, folder, leftOrRight)
+        print(folder)
+        count = 0
         if os.path.exists(folder):
             for img_name in sorted(os.listdir(folder)):
                 if img_name != "Thumbs.db":  # Exclude Thumbs.db
                     img_path = os.path.join(folder, img_name)
+                    print(img_path)
                     img = load_img(
                         img_path,
                         target_size=img_size,
                         color_mode="grayscale" if grayscale else "rgb",
                     )
+                    count += 1
                     img_array = img_to_array(img)  # Convert image to NumPy array
                     images.append(img_array)
             # Try to load corresponding mask (from L01 to L10)
-            for mask_suffix in range(1, 11):  # Assuming masks can be L01 to L10
+            for mask_suffix in range(1, count + 1):  # Assuming masks can be L01 to L10
                 mask_name = (
                     "OperatorA_" + f"S1{i:03d}{leftOrRight}{mask_suffix:02d}.tiff"
                 )
                 mask_path = os.path.join(maskFolder, mask_name)
                 if os.path.exists(mask_path):
+                    print(mask_path)
                     mask = load_img(
                         mask_path,
                         target_size=img_size,
