a5df02494d3168658adcb4a7bab31e23fae1bc74
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-10-02 12:07:32 +0530

    updated output path

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ save_predictions = model.save_predictions
 
 # Define output folder for predictions
 if dataLoader.leftOrRight == 'L':
-    output_folder = 'IrisDataset/Left Eye/Left Eye Output'
+    output_folder = '/content/drive/MyDrive/Iris-Output/Left Eye'
 else:
-    output_folder = 'IrisDataset/Right Eye/Right Eye Output'  # Replace with your desired output folder path
+    output_folder = '/content/drive/MyDrive/Iris-Output/Right Eye'  # Replace with your desired output folder path
 save_predictions(predictions, output_folder)
